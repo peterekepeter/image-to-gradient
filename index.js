891ae69a12b3a999c707576a580bb2a8f8ae265b
Eke Peter <peterekepeter@gmail.com>
2017-03-21 20:42:00 +0200

    refactored project to make it testable

--- index.js
+++ index.js
@@ -1,5 +1,86 @@
 
 
+// create a new gradient object with fixed amount of steps
+function createGradient(steps, angle){
+    var gradient = { steps:steps, red:[], green:[], blue:[], alpha:[], unit:[], angle:angle };
+    for (var i=0; i<gradient.steps; i++)
+    {
+        gradient.red[i] = 0;
+        gradient.green[i] = 0;
+        gradient.blue[i] = 0;
+        gradient.alpha[i] = 0;
+        gradient.unit[i] = 0;
+    }
+    return gradient;
+}
+
+// normalize a gradient
+function normalizeGradient(gradient){
+    // divide by unit
+    for (var i=0; i<gradient.steps; i++)
+    {
+        var unit = gradient.unit[i];
+        gradient.red[i] /= unit;
+        gradient.green[i] /= unit;
+        gradient.blue[i] /= unit;
+        gradient.alpha[i] /= unit;
+        gradient.unit[i] = 1;
+    }
+}
+
+// add color to gradient
+function addToGradient(gradient, index, red, green, blue, alpha, weight){   
+    var y = index;
+    gradient.red[y] += red;
+    gradient.green[y] += green;
+    gradient.blue[y] += blue;
+    gradient.alpha[y] += alpha;
+    gradient.unit[y] += weight;
+}
+
+function fmod(a,b) { return Number((a - (Math.floor(a / b) * b))); }
+
+// reduce image to gradient
+function reduce(image, steps, angle){
+    var gradient = createGradient(steps, angle);
+    var resized = image.resize(steps, steps);
+    var cos = Math.cos(angle/180.0*Math.PI);
+    var sin = Math.sin(angle/180.0*Math.PI);
+    var fsteps = gradient.steps;
+    var hsteps = fsteps * 0.5;
+    resized.scan(0, 0, resized.bitmap.width, resized.bitmap.height, function(x, y, idx){
+        for (var i=0; i<gradient.steps; i++){
+            var weight = fmod(sin*x + cos*y - i + hsteps, fsteps) - hsteps;
+            weight = 1.0 - Math.abs(weight);
+            if (weight <= 0) continue;
+            addToGradient(gradient, i, resized.bitmap.data[idx]*weight, resized.bitmap.data[idx+1]*weight, resized.bitmap.data[idx+2]*weight, resized.bitmap.data[idx+3]*weight, weight);
+        }
+    });
+    return gradient;
+}
+
+// convert a gradient to css gradient string
+function gradientToCssString(gradient){
+    // build css gradient
+    var str = ['linear-gradient'];
+    var separator = '(';
+    if(gradient.angle !== 0){
+        separator = `(${gradient.angle}deg,`;
+    }
+    for (i=0; i<gradient.steps; i++)
+    {
+        var red = Math.floor(gradient.red[i]);
+        var green = Math.floor(gradient.green[i]);
+        var blue = Math.floor(gradient.blue[i]);
+        var alpha = (gradient.alpha[i]/255.0).toPrecision(2); 
+        str.push(`${separator}rgba(${red},${green},${blue},${alpha})`);
+        separator = ',';
+    }
+    str.push(')');
+    return str.join('');
+}
+
+
 
 function imageToGradient(imagePath, options, callback){
 
@@ -12,90 +93,6 @@ function imageToGradient(imagePath, options, callback){
     if (optionSteps == null) optionSteps = 16;
 
 
-    //helpers
-
-    // create a new gradient object with fixed amount of steps
-    function createGradient(steps, angle){
-        var gradient = { steps:steps, red:[], green:[], blue:[], alpha:[], unit:[], angle:angle };
-        for (var i=0; i<gradient.steps; i++)
-        {
-            gradient.red[i] = 0;
-            gradient.green[i] = 0;
-            gradient.blue[i] = 0;
-            gradient.alpha[i] = 0;
-            gradient.unit[i] = 0;
-        }
-        return gradient;
-    }
-
-    // normalize a gradient
-    function normalizeGradient(gradient)
-    {
-        // divide by unit
-        for (var i=0; i<gradient.steps; i++)
-        {
-            var unit = gradient.unit[i];
-            gradient.red[i] /= unit;
-            gradient.green[i] /= unit;
-            gradient.blue[i] /= unit;
-            gradient.alpha[i] /= unit;
-            gradient.unit[i] = 1;
-        }
-    }
-
-    // add color to gradient
-    function addToGradient(gradient, index, red, green, blue, alpha, weight)
-    {   
-        var y = index;
-        gradient.red[y] += red;
-        gradient.green[y] += green;
-        gradient.blue[y] += blue;
-        gradient.alpha[y] += alpha;
-        gradient.unit[y] += weight;
-    }
-
-    function fmod(a,b) { return Number((a - (Math.floor(a / b) * b))); }
-
-    // reduce image to gradient
-    function reduce(image, steps, angle)
-    {
-        var gradient = createGradient(steps, angle);
-        var resized = image.resize(steps, steps);
-        var cos = Math.cos(angle/180.0*Math.PI);
-        var sin = Math.sin(angle/180.0*Math.PI);
-        var fsteps = gradient.steps;
-        var hsteps = fsteps * 0.5;
-        resized.scan(0, 0, resized.bitmap.width, resized.bitmap.height, function(x, y, idx){
-            for (var i=0; i<gradient.steps; i++){
-                var weight = fmod(sin*x + cos*y - i + hsteps, fsteps) - hsteps;
-                weight = 1.0 - Math.abs(weight);
-                if (weight <= 0) continue;
-                addToGradient(gradient, i, resized.bitmap.data[idx]*weight, resized.bitmap.data[idx+1]*weight, resized.bitmap.data[idx+2]*weight, resized.bitmap.data[idx+3]*weight, weight);
-            }
-        });
-        return gradient;
-    }
-
-    // convert a gradient to css gradient string
-    function gradientToCssString(gradient){
-        // build css gradient
-        var str = ['linear-gradient'];
-        var separator = '(';
-        if(gradient.angle !== 0){
-            separator = `(${gradient.angle}deg,`;
-        }
-        for (i=0; i<gradient.steps; i++)
-        {
-            var red = Math.floor(gradient.red[i]);
-            var green = Math.floor(gradient.green[i]);
-            var blue = Math.floor(gradient.blue[i]);
-            var alpha = (gradient.alpha[i]/255.0).toPrecision(2); 
-            str.push(`${separator}rgba(${red},${green},${blue},${alpha})`);
-            separator = ',';
-        }
-        str.push(')');
-        return str.join('');
-    }
 
     var Jimp = require("jimp");
     
@@ -113,4 +110,14 @@ function imageToGradient(imagePath, options, callback){
 
 module.exports = imageToGradient;
 var exports = module.exports; //redefine exports
-exports.imageToGradient = imageToGradient; // backwards compatilibity
+
+// backwards compatilibity
+exports.imageToGradient = imageToGradient; 
+
+// export helper functions to make it test-able
+exports.gradientToCssString = gradientToCssString;
+exports.reduce = reduce;
+exports.fmod = fmod;
+exports.addToGradient = addToGradient;
+exports.normalizeGradient = normalizeGradient;
+exports.createGradient = createGradient;
---
+++ test/test.js
@@ -0,0 +1,30 @@
+
+
+
+var assert = require('assert');
+var imageToGradient = require('../');
+
+describe('image-to-gradient', function() {
+  describe('fmod', function() {
+    it('fmod(1,1) should return 0', function() {
+      assert.equal(0, imageToGradient.fmod(1,1));
+    });
+    it('fmod(-1,1) should return 0', function() {
+      assert.equal(0, imageToGradient.fmod(-1,1));
+    });
+    it('always return a number less than the second parameter', function() {
+      var x,y;
+      for (x=1; x<10; x+=1){
+        for (y=1; y<10; y+=1){
+            assert(imageToGradient.fmod(x,y) < y);
+        }
+      }
+    });
+    it('should always return a positive number', function(){
+        for(var x=-2; x<2; x+=0.1){
+            assert(imageToGradient.fmod(x, 1) >= 0);
+        }
+    });
+  });
+});
+
