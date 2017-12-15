0ebec448a882cf2aec0ce028eb4ae92f190326e1
Adam Ydenius <mollwe@gmail.com>
2017-12-15 02:07:49 +0100

    Add sample tests and fix issue with angle in gradient string

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 
 
 // create a new gradient object with fixed amount of steps
-function createGradient(steps, angle){
-    var gradient = { steps:steps, red:[], green:[], blue:[], alpha:[], unit:[], angle:angle };
-    for (var i=0; i<gradient.steps; i++)
-    {
+function createGradient(steps, angle) {
+    var gradient = { steps: steps, red: [], green: [], blue: [], alpha: [], unit: [], angle: angle };
+    for (var i = 0; i < gradient.steps; i++) {
         gradient.red[i] = 0;
         gradient.green[i] = 0;
         gradient.blue[i] = 0;
@@ -15,10 +14,9 @@ function createGradient(steps, angle){
 }
 
 // normalize a gradient
-function normalizeGradient(gradient){
+function normalizeGradient(gradient) {
     // divide by unit
-    for (var i=0; i<gradient.steps; i++)
-    {
+    for (var i = 0; i < gradient.steps; i++) {
         var unit = gradient.unit[i];
         gradient.red[i] /= unit;
         gradient.green[i] /= unit;
@@ -29,7 +27,7 @@ function normalizeGradient(gradient){
 }
 
 // add color to gradient
-function addToGradient(gradient, index, red, green, blue, alpha, weight){   
+function addToGradient(gradient, index, red, green, blue, alpha, weight) {
     var y = index;
     gradient.red[y] += red;
     gradient.green[y] += green;
@@ -38,42 +36,41 @@ function addToGradient(gradient, index, red, green, blue, alpha, weight){
     gradient.unit[y] += weight;
 }
 
-function fmod(a,b) { return Number((a - (Math.floor(a / b) * b))); }
+function fmod(a, b) { return Number((a - (Math.floor(a / b) * b))); }
 
 // reduce image to gradient
-function reduce(image, steps, angle){
+function reduce(image, steps, angle) {
     var gradient = createGradient(steps, angle);
     var resized = image.resize(steps, steps);
-    var cos = Math.cos(angle/180.0*Math.PI);
-    var sin = Math.sin(angle/180.0*Math.PI);
+    var cos = Math.cos(angle / 180.0 * Math.PI);
+    var sin = Math.sin(angle / 180.0 * Math.PI);
     var fsteps = gradient.steps;
     var hsteps = fsteps * 0.5;
-    resized.scan(0, 0, resized.bitmap.width, resized.bitmap.height, function(x, y, idx){
-        for (var i=0; i<gradient.steps; i++){
-            var weight = fmod(sin*x + cos*y - i + hsteps, fsteps) - hsteps;
+    resized.scan(0, 0, resized.bitmap.width, resized.bitmap.height, function (x, y, idx) {
+        for (var i = 0; i < gradient.steps; i++) {
+            var weight = fmod(sin * x + cos * y - i + hsteps, fsteps) - hsteps;
             weight = 1.0 - Math.abs(weight);
             if (weight <= 0) continue;
-            addToGradient(gradient, i, resized.bitmap.data[idx]*weight, resized.bitmap.data[idx+1]*weight, resized.bitmap.data[idx+2]*weight, resized.bitmap.data[idx+3]*weight, weight);
+            addToGradient(gradient, i, resized.bitmap.data[idx] * weight, resized.bitmap.data[idx + 1] * weight, resized.bitmap.data[idx + 2] * weight, resized.bitmap.data[idx + 3] * weight, weight);
         }
     });
     return gradient;
 }
 
 // convert a gradient to css gradient string
-function gradientToCssString(gradient){
+function gradientToCssString(gradient) {
     // build css gradient
     var str = ['linear-gradient'];
     var separator = '(';
-    if(gradient.angle !== 0){
-        separator = '('+gradient.angle+'deg';
+    if (gradient.angle !== 0) {
+        separator = '(' + gradient.angle + 'deg,';
     }
-    for (i=0; i<gradient.steps; i++)
-    {
+    for (i = 0; i < gradient.steps; i++) {
         var red = Math.floor(gradient.red[i]);
         var green = Math.floor(gradient.green[i]);
         var blue = Math.floor(gradient.blue[i]);
-        var alpha = (gradient.alpha[i]/255.0).toPrecision(2); 
-        str.push(separator+'rgba('+red+','+green+','+blue+','+alpha+')');
+        var alpha = (gradient.alpha[i] / 255.0).toPrecision(2);
+        str.push(separator + 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')');
         separator = ',';
     }
     str.push(')');
@@ -82,7 +79,7 @@ function gradientToCssString(gradient){
 
 
 
-function imageToGradient(imagePath, options, callback){
+function imageToGradient(imagePath, options, callback) {
 
     if (options == null) options = {};
 
@@ -113,7 +110,7 @@ module.exports = imageToGradient;
 var exports = module.exports; //redefine exports
 
 // backwards compatilibity
-exports.imageToGradient = imageToGradient; 
+exports.imageToGradient = imageToGradient;
 
 // export helper functions to make it test-able
 exports.gradientToCssString = gradientToCssString;
--- test/test.js
+++ test/test.js
@@ -1,27 +1,34 @@
 
 
 var blanket = require('blanket')({
-    pattern:'/'
+    pattern: '/'
 });
 
 var assert = require('assert');
 var imageToGradient = require('../');
 
-describe('image-to-gradient', function() {
-  describe('fmod', function() {
-    it('fmod(1,1) should return 0', function() {
-      assert.equal(0, imageToGradient.fmod(1,1));
+describe('image-to-gradient', function () {
+    describe('fmod', function () {
+        it('fmod(1,1) should return 0', function () {
+            assert.equal(0, imageToGradient.fmod(1, 1));
+        });
+        it('fmod(-1,1) should return 0', function () {
+            assert.equal(0, imageToGradient.fmod(-1, 1));
+        });
+        it('always return a number less than the second parameter', function () {
+            var x, y;
+            for (x = 1; x < 10; x += 1) {
+                for (y = 1; y < 10; y += 1) {
+                    assert(imageToGradient.fmod(x, y) < y);
+                }
+            }
+        });
+        it('should always return a positive number', function () {
+            for (var x = -2; x < 2; x += 0.1) {
+                assert(imageToGradient.fmod(x, 1) >= 0);
+            }
+        });
     });
-    it('fmod(-1,1) should return 0', function() {
-      assert.equal(0, imageToGradient.fmod(-1,1));
-    });
-    it('always return a number less than the second parameter', function() {
-      var x,y;
-      for (x=1; x<10; x+=1){
-        for (y=1; y<10; y+=1){
-            assert(imageToGradient.fmod(x,y) < y);
-        }
-      }
 
     describe('callback', function () {
         it('should execute only once', function (done) {
@@ -58,11 +65,78 @@ describe('image-to-gradient', function() {
             });
         });
     });
-    it('should always return a positive number', function(){
-        for(var x=-2; x<2; x+=0.1){
-            assert(imageToGradient.fmod(x, 1) >= 0);
-        }
+
+    describe('samples', function () {
+        it('should resolve top white to black in 2 steps as white to black', function (done) {
+            imageToGradient('test/images/T-FFF_000.png', { steps: 2, angle: 0 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(rgba(255,255,255,1.0),rgba(0,0,0,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve top white to black in 2 steps with 90deg as grey', function (done) {
+            imageToGradient('test/images/T-FFF_000.png', { steps: 2, angle: 90 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(90deg,rgba(127,127,127,1.0),rgba(127,127,127,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve top white to black in 10 steps as white to black with distinct center', function (done) {
+            imageToGradient('test/images/T-FFF_000.png', { steps: 10, angle: 0 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve left black to white in 2 steps as black to white', function (done) {
+            imageToGradient('test/images/L-000_FFF.png', { steps: 2, angle: 90 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(90deg,rgba(0,0,0,1.0),rgba(255,255,255,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve left black to white in 10 steps as black to white with distinct center', function (done) {
+            imageToGradient('test/images/L-000_FFF.png', { steps: 10, angle: 90 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(90deg,rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(0,0,0,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0),rgba(255,255,255,1.0))', 'result was undefined');
+                done();
+            });
+        });
+        //------------
+        it('should resolve bottom rainbow in 2 steps into two average colors', function (done) {
+            imageToGradient('test/images/B-E50022_E04000_DC9E00_B7D800_57D400_00CF04_00CB5C_00C7B1_0083C3_002EBF.png', { steps: 2, angle: 180 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(180deg,rgba(0,157,132,1.0),rgba(189,130,7,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve bottom rainbow in 2 steps with 270deg a single average color', function (done) {
+            imageToGradient('test/images/B-E50022_E04000_DC9E00_B7D800_57D400_00CF04_00CB5C_00C7B1_0083C3_002EBF.png', { steps: 2, angle: 270 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(270deg,rgba(94,143,69,1.0),rgba(94,143,69,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve bottom rainbow in 10 steps into a smoother average color gradient', function (done) {
+            imageToGradient('test/images/B-E50022_E04000_DC9E00_B7D800_57D400_00CF04_00CB5C_00C7B1_0083C3_002EBF.png', { steps: 10, angle: 180 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(180deg,rgba(0,46,191,1.0),rgba(228,0,34,1.0),rgba(224,63,0,1.0),rgba(220,158,0,1.0),rgba(183,216,0,1.0),rgba(87,212,0,1.0),rgba(0,207,4,1.0),rgba(0,203,92,1.0),rgba(0,199,177,1.0),rgba(0,131,194,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve right rainbow in 2 steps into two average colors', function (done) {
+            imageToGradient('test/images/R-E50022_E04000_DC9E00_B7D800_57D400_00CF04_00CB5C_00C7B1_0083C3_002EBF.png', { steps: 2, angle: 270 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(270deg,rgba(0,157,132,1.0),rgba(189,130,7,1.0))', 'result was undefined');
+                done();
+            });
+        });
+
+        it('should resolve right rainbow to white in 10 steps into a smoother average color gradient', function (done) {
+            imageToGradient('test/images/R-E50022_E04000_DC9E00_B7D800_57D400_00CF04_00CB5C_00C7B1_0083C3_002EBF.png', { steps: 10, angle: 270 }, function (error, result) {
+                assert.equal(result, 'linear-gradient(270deg,rgba(0,45,190,1.0),rgba(229,0,33,1.0),rgba(223,64,0,1.0),rgba(219,158,0,1.0),rgba(183,216,0,1.0),rgba(86,212,0,1.0),rgba(0,207,4,1.0),rgba(0,203,92,1.0),rgba(0,198,177,1.0),rgba(0,130,195,1.0))', 'result was undefined');
+                done();
+            });
+        });
     });
-  });
+
 });
 
